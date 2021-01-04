c50528898e196de1c534535b81b31c371dd14f0a
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2021-01-04 09:35:54 +0200

    Pirmasis pakeitimas

--- src/AntraPamoka.java
+++ src/AntraPamoka.java
@@ -3,5 +3,10 @@ public class AntraPamoka
     public static void main(String[] args)
     {
         System.out.println("testas");
+        System.out.println("testas");
+        System.out.println("testas");
+        System.out.println("testas");
+        System.out.println("testas");
+        System.out.println("testas");
     }
 }
